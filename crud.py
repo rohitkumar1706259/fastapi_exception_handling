ad7d54ab171565b5e233bd1d27c310d753fa595a
1706259 <rohitkumar.geekit@gmail.com>
2021-01-18 15:21:25 +0530

    All files for exception handling

---
+++ crud.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+
+def get_one_items(db: Session, id:id):
+    return db.query(models.Item).filter(models.Item.id == id).all()
+
+def create_user_item(db: Session, item: schemas.ItemCreate):
+    db_item = models.Item(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
